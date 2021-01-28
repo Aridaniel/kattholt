e22fa0d9c61c4bc8d437a65cbaaa60430c987fd3
Hulda <huldas96@gmail.com>
2021-01-28 16:29:13 +0000

    work

--- src/components/CatProfileModal.js
+++ src/components/CatProfileModal.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Modal from 'react-modal';
 
 // cat photo
@@ -6,51 +6,64 @@ const url =
   'https://ichef.bbci.co.uk/news/976/cpsprodpb/41CF/production/_109474861_angrycat-index-getty3-3.jpg';
 
 function CatProfileModal() {
+  const [content, setContent] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      const response = await fetch("http://localhost:4000/api");
+      const data = await response.json();
+      setContent(data);
+    }
+
+    fetchData();
+  },[]);
+
   var subtitle;
   const [modalIsOpen, setIsOpen] = useState(false);
   function openModal() {
     setIsOpen(true);
   }
 
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00';
-  }
 
   function closeModal() {
     setIsOpen(false);
   }
   return (
     <>
-      <button className='cat-container' onClick={openModal}>
+    {/* This is the button that opens the modal/ cat profile  */}
+    {content.map((i)=>{
+      return(
+        <button className='cat-container' onClick={openModal}>
         <div>
           <img src={url} alt='' />
           <div className='cat-info'>
             <div className='name'>
-              <h1>Siggi</h1>
-              <p>5 ára</p>
+              <h1>{i.heitiKattar}</h1>
+              <p>{i.aldur}</p>
             </div>
-            <div className='ormerki'>123423234234</div>
+            <div className='ormerki'>{i.ormerking}</div>
           </div>
         </div>
       </button>
+      )
+    })}
 
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         contentLabel='Example Modal'
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello</h2>
         <button onClick={closeModal}>close</button>
-        <div>I am a modal</div>
-        <form>
-          <input />
-          <button>tab navigation</button>
-          <button>stays</button>
-          <button>inside</button>
-          <button>the modal</button>
-        </form>
+        <div key={content._id}>
+          <img src={url} alt='' />
+          <div className='cat-info'>
+            <div className='name'>
+              <h1>{content.heitiKattar}</h1>
+              <p>{content.aldur}</p>
+            </div>
+            <div className='ormerki'>{content.ormerking}</div>
+          </div>
+        </div>
       </Modal>
     </>
   );
