2d805dd94a84a85556cdf4e0093fa3d8bd415fb8
Daniel Bergmann <dba222@outlook.com>
2021-01-28 14:09:39 +0000

    added react router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+// importing Dashboard
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      Kitty-katt
-      fullt af kitty kaaatt
+    <Router>
+      <div>
         
-    </div>
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path='/dashboard'>
+            <Dashboard />
+          </Route>
+          <Route path='/'>
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return (
+    <>
+      <h1>hallo</h1>
+      <Login />
+    </>
+  );
+}
