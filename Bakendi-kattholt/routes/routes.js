614c45de4d233d901adf0359a9ffdc81cdcb6d6b
Daniel Bergmann <dba222@outlook.com>
2021-02-03 12:35:08 +0000

    added comments to the codebase

--- Bakendi-kattholt/routes/routes.js
+++ Bakendi-kattholt/routes/routes.js
@@ -1,45 +1,23 @@
 const express = require('express');
 const router = express.Router();
+// importing the Schema to each route
 const kisi = require('../models/kottur');
 const mongoose = require('mongoose');
-// const ObjectId = require('mongoose').Types.ObjectId;
-
-
-
-
-
-
 
 router.delete('/kittyprofile/:idKottur', (req, res) => {
-  
-  // const deletekisi = new kisi();
-  
   const { idKottur } = req.params;
-  console.log(JSON.stringify(idKottur));
-
   kisi.find({ _id: idKottur }).remove(function (err) {
     if (err) console.log(err);
   });
   res.send(`Cat with the ormerking ${idKottur} was deleted from the database`);
 });
 
-
-
-
-
 // Add a new cat to the DB
-
 router.post('/kittyprofile', (request, response) => {
   kisi.create(request.body);
 });
 
-
-
-
-
-
-// Get data Cats from DAtabase
-
+// Get data Cats from DB and display on the client side
 router.get('/api', async (req, res) => {
   // Turns the MongoDB data in to an array
   const data = await kisi.find({}, (err, kisur) => {
@@ -48,25 +26,12 @@ router.get('/api', async (req, res) => {
   });
 });
 
-
-
-
-// Update CAt
-
-
+// Update Cat info in the DB
 router.put('/kittyprofile/:idKottur', (req, res) => {
-
-
   const { idKottur } = req.params;
-  console.log(JSON.stringify(idKottur));
-  kisi.updateOne({ _id: idKottur }, req.body)
-  .then(()=>{
-    res.send('Changing data')
-    console.log(req.body)
-
-  })
-  
+  kisi.updateOne({ _id: idKottur }, req.body).then(() => {
+    res.send('Changing data');
+  });
 });
-
 
 module.exports = router;
--- Bakendi-kattholt/server.js
+++ Bakendi-kattholt/server.js
@@ -3,21 +3,21 @@ const express = require('express');
 const app = express();
 // Library for the MONGO DB
 const mongoose = require('mongoose');
-// This is for the CRUD to work
+// This is for the CRUD to work and hide the DB username and password
 const dotenv = require('dotenv');
 dotenv.config();
 // Importing routes for the API data
 const routesUrls = require('./routes/routes');
-//import routesUrls from "routes"
+// Cors is for security
 const cors = require('cors');
-// connecting the database
+// connecting the database through mongoose
 mongoose.connect(
   process.env.DATABASE_ACCESS,
   { useNewUrlParser: true, useUnifiedTopology: true },
   () => console.log('Database is connected')
 );
 
-
+// Connectiong Cors for security
 app.use(express.json());
 app.use(cors());
 
